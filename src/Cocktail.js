4a3000691ab7c8e2bc62ffe9341e63e23196bb2c
Дарья Соловьева <dariasoloveva@Darias-New-MacBook-Pro.local>
2020-09-25 14:18:15 +0400

    add react-snap

--- src/Cocktail.js
+++ src/Cocktail.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import { Share } from "./Share";
 
@@ -18,6 +19,17 @@ export const Cocktail = () => {
 
   return (
     <div>
+      <Helmet>
+        <meta name="twitter:card" content="summary"></meta>
+        <meta name="twitter:title" content="Cocktails App" />
+        <meta name="twitter:description" content="Let's shake it" />
+        <meta name="twitter:image" content={cocktail.image} />
+
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="Cocktails App" />
+        <meta property="og:description" content="Let's shake it" />
+        <meta property="og:image" content={cocktail.image} />
+      </Helmet>
       <h1>{cocktail.name}</h1>
       <img src={cocktail.image} alt={cocktail.name} />
       <p>{cocktail.description}</p>
--- src/Share.js
+++ src/Share.js
@@ -1,5 +1,29 @@
 import React from "react";
 
+const handleShareFacebook = () => {
+  window.open(
+    "http://www.facebook.com/sharer.php?u=https://dsolovieva.github.io/react-share-demo/#/cocktail/1",
+    "pop",
+    "width=600, height=400, scrollbars=no"
+  );
+};
+
+const handleShareTwitter = () => {
+  window.open(
+    "http://twitter.com/intent/tweet?text=https://dsolovieva.github.io/react-share-demo/#/cocktail/1",
+    "pop",
+    "width=600, height=400, scrollbars=no"
+  );
+};
+
+const handleSharePinterest = () => {
+  window.open(
+    "http://pinterest.com/pin/create/link/?url=https://dsolovieva.github.io/react-share-demo/#/cocktail/1&media=https://static.vinepair.com/wp-content/uploads/2020/04/bloodandsand_internal.jpg&description=hello",
+    "pop",
+    "width=600, height=400, scrollbars=no"
+  );
+};
+
 export const Share = () => {
   return (
     <div>
@@ -9,6 +33,9 @@ export const Share = () => {
         </a>
       </div>
       <div>
+        <button onClick={handleShareTwitter}>Twitter</button>
+      </div>
+      <div>
         <a
           href={`http://www.facebook.com/sharer.php?u=https://dsolovieva.github.io/react-share-demo/#/cocktail/1`}
         >
@@ -16,9 +43,15 @@ export const Share = () => {
         </a>
       </div>
       <div>
+        <button onClick={handleShareFacebook}>Facebook</button>
+      </div>
+      <div>
         <a href="http://pinterest.com/pin/create/link/?url=https://dsolovieva.github.io/react-share-demo/#/cocktail/1&media=https://static.vinepair.com/wp-content/uploads/2020/04/bloodandsand_internal.jpg&description=hello">
           Pinterest
         </a>
+      </div>
+      <div>
+        <button onClick={handleSharePinterest}>Pinterest</button>
       </div>
     </div>
   );
